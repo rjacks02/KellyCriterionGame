45a9e00eb5dd9799b8a25a979942e9d500dc59c3
rjacks02 <rjacks02@villanova.edu>
2024-07-03 01:26:06 -0400

    new repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import Intro from './pages/Intro.js';
+import Game from './pages/Game.js';
+import End from './pages/End.js';
+import styles from './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+      <h1 class = 'Main'>The Updated Kelly Criterion Investment Game</h1>
+    <HashRouter>
+    <Routes>
+      <Route index element={<Intro />} />
+      <Route path = 'Intro' element={<Intro />} />
+      <Route path = 'Game' element={<Game />} />
+      <Route path = 'End' element={<End />} />
+    </Routes>
+  </HashRouter>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
